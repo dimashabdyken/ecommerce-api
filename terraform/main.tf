d4f00edeaf7a23b9da5a65e5aa734471d0165016
dimashabdyken <abdykendinmuhammed@gmail.com>
2026-04-02 19:53:27 +0500

    Fix: reverted tp the correct block form

--- terraform/main.tf
+++ terraform/main.tf
@@ -82,19 +82,17 @@ resource "aws_s3_bucket_public_access_block" "terraform_state" {
   restrict_public_buckets = true
 }
 
-# Получаем временный токен для авторизации в созданном EKS
 data "aws_eks_cluster_auth" "cluster" {
   name = aws_eks_cluster.this.name
 }
 
-# Настраиваем провайдер Kubernetes
 provider "kubernetes" {
   host                   = aws_eks_cluster.this.endpoint
   cluster_ca_certificate = base64decode(aws_eks_cluster.this.certificate_authority[0].data)
   token                  = data.aws_eks_cluster_auth.cluster.token
 }
 
-# Настраиваем провайдер Helm
+
 provider "helm" {
   kubernetes {
     host                   = aws_eks_cluster.this.endpoint
